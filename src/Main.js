7b94877f97f9753cd65af5d54da8d2cec7311c49
Ricardo López Álvarez <zionlsr@gmail.com>
2022-09-23 12:00:38 +0200

    Add global state for show/hidden to do's completed

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,38 @@
+import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Navigation from './components/Navigation'
-import ToDoForm from './components/ToDoForm'
-import ToDoList from './components/ToDoList'
+import Navigation from './components/navigation/Navigation'
+import ToDoForm from './components/todoform/ToDoForm'
+import ToDoList from './components/todolist/ToDoList'
 
 function Main() {
+	const [showCompletedToDos, setShowCompletedToDos] = useState(false)
+
+	const handleToggleShowCompletedToDos = () =>
+		setShowCompletedToDos(!showCompletedToDos)
+
 	return (
-		<div className='bg-zinc-900 h-screen text-white mx-auto p-5 sm:p-10 md:p-15'>
+		<div className='bg-zinc-900 min-h-screen text-white mx-auto p-5 sm:p-10 md:p-15'>
 			<BrowserRouter>
-				<div className='grid grid-cols-2 items-center mb-8'>
-					<h1 className='text-4xl font-light justify-self-start w-max uppercase'>
-						to do list
+				<header className='grid grid-cols-2 items-center mb-4'>
+					<h1 className='text-4xl font-light justify-self-start w-max'>
+						TO DO LIST
 					</h1>
 					<Navigation />
-				</div>
+				</header>
 				<Routes>
-					<Route path='/' element={<ToDoList />} exact />
+					<Route
+						path='/'
+						element={
+							<ToDoList
+								showCompletedToDos={showCompletedToDos}
+								handleToggleShowCompletedToDos={
+									handleToggleShowCompletedToDos
+								}
+							/>
+						}
+						exact
+					/>
 					<Route path='/create' element={<ToDoForm />} exact />
 					<Route path='/update/:id' element={<ToDoForm />} exact />
 				</Routes>
