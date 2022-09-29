6ad2074838b3def75978045c7a32b1cbabf2736f
Ricardo López Álvarez <zionlsr@gmail.com>
2022-09-29 15:23:16 +0200

    Refactor

--- src/components/todolist/ToDoList.js
+++ src/components/todolist/ToDoList.js
@@ -1,55 +1,68 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { deleteToDo, readToDos } from '../../features/todos/toDoSlice'
+import { deleteToDo } from '../../features/todos/toDoSlice'
 import ToDoListContainer from './ToDoListContainer'
 import ToDoShowCompletedButton from './ToDoShowCompletedButton'
 
 const ToDoList = ({ showCompletedToDos, handleToggleShowCompletedToDos }) => {
 	const dispatch = useDispatch()
 	const toDosState = useSelector(state => state.toDos)
-	const toDosPending = toDosState
-		.filter(toDo => toDo.completed === false)
-		.sort((a, b) => {
-			if (a.priority < b.priority) return -1
-			if (a.priority > b.priority) return 1
-			return 0
-		})
-	const toDosCompleted = toDosState
-		.filter(toDo => toDo.completed === true)
-		.sort((a, b) => {
-			if (a.priority < b.priority) return -1
-			if (a.priority > b.priority) return 1
-			return 0
-		})
+	const [toDosPending, setToDosPending] = useState()
+	const [toDosCompleted, setToDosCompleted] = useState()
 
 	useEffect(() => {
-		if (toDosState.length === 0) {
-			dispatch(readToDos())
+		if (toDosState) {
+			setToDosPending(
+				toDosState
+					.filter(toDo => toDo.completed === false)
+					.sort((a, b) => {
+						if (a.priority < b.priority) return -1
+						if (a.priority > b.priority) return 1
+						return 0
+					})
+			)
+			setToDosCompleted(
+				toDosState
+					.filter(toDo => toDo.completed === true)
+					.sort((a, b) => {
+						if (a.priority < b.priority) return -1
+						if (a.priority > b.priority) return 1
+						return 0
+					})
+			)
 		}
-	}, [dispatch, toDosState])
+	}, [toDosState])
 
 	const handleDelete = id => dispatch(deleteToDo(id))
 
-	return (
-		<>
-			<ToDoShowCompletedButton
-				handleToggleShowCompletedToDos={handleToggleShowCompletedToDos}
-				show={showCompletedToDos}
-			/>
-			<ToDoListContainer
-				toDos={toDosPending}
-				type='pending'
-				handleDelete={handleDelete}
-			/>
-			<ToDoListContainer
-				toDos={toDosCompleted}
-				type='completed'
-				show={showCompletedToDos}
-				handleDelete={handleDelete}
-			/>
-		</>
-	)
+	let content = <p>Loading...</p>
+
+	if (toDosPending && toDosCompleted) {
+		content = (
+			<>
+				<ToDoShowCompletedButton
+					handleToggleShowCompletedToDos={
+						handleToggleShowCompletedToDos
+					}
+					show={showCompletedToDos}
+				/>
+				<ToDoListContainer
+					toDos={toDosPending}
+					type='pending'
+					handleDelete={handleDelete}
+				/>
+				<ToDoListContainer
+					toDos={toDosCompleted}
+					type='completed'
+					show={showCompletedToDos}
+					handleDelete={handleDelete}
+				/>
+			</>
+		)
+	}
+
+	return <>{content}</>
 }
 
 export default ToDoList
